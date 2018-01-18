0dc9edd5accc092320928fa5f6706d5536610afb
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2018-01-19 00:44:24 +0200

    Implememted two XPath's as options with defaults coming via message from background page

--- ext/scripts/options.js
+++ ext/scripts/options.js
@@ -4,9 +4,11 @@ console.log("options.js");
 
 function save_options(evt) {
   evt.preventDefault();
-  var url = document.forms.options.url.value;
+  var xp_count_elm = document.forms.options.xp_count_elm.value;
+  var xp_folders = document.forms.options.xp_folders.value;
   chrome.storage.sync.set({
-    url: url
+    xp_count_elm: xp_count_elm,
+    xp_folders: xp_folders
   }, function() {
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
@@ -16,13 +18,31 @@ function save_options(evt) {
   });
 }
 
+// function restore_options() {
+//   chrome.storage.sync.get({
+//     xp_count_elm:
+//      "//div[@id='MailFolderPane.FavoritesFolders']//span[text()='Inbox']/following-sibling::*[1]/span[1]",
+//     xp_folders:
+//      "//div[@class='subfolders' and @role='group']/..//span[@role='heading' and @autoid]"
+//   }, function(items) {
+//     document.forms.options.xp_count_elm.value = items.xp_count_elm;
+//     document.forms.options.xp_folders.value = items.xp_folders;
+//   });
+// }
 
 function restore_options() {
-  chrome.storage.sync.get({
-    url: ''
-  }, function(items) {
-    document.forms.options.url.value = items.url;
-  });
+  chrome.runtime.sendMessage(
+    {action: 'defaults'}, function(response) {
+      let xp_count_elm = response.xp_count_elm;
+      let xp_folders = response.xp_folders;
+      chrome.storage.sync.get(
+          {xp_count_elm: response.xp_count_elm,
+           xp_folders: response.xp_folders},
+      function(items) {
+        document.forms.options.xp_count_elm.value = items.xp_count_elm;
+        document.forms.options.xp_folders.value = items.xp_folders;
+      });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
